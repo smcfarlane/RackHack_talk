1529aed232d9b8c129fecbca32f613cc4cba49db
stevenmcfarlane <stevemac72@gmail.com>
2017-04-01 14:50:48 -0700

    fixes and updates

--- app/middleware/append_to_body.rb
+++ app/middleware/append_to_body.rb
@@ -1,15 +1,24 @@
 
 module Slides
-  class AppendToBody
-    def initialize(app, value)
-      @app = app
-      @value = value
-    end
 
-    def call(env)
-      status, headers, body = @app.call(env)
-      content = body.last + @value
-      [status, headers, [content]]
-    end
+
+
+class AppendToBody
+  def initialize(app, value)
+    @app = app
+    @value = value
   end
+
+  def call(env)
+    env['AppendToBody'] = @value
+
+    status, headers, body = @app.call(env)
+    
+    content = body.last + @value
+    [status, headers, [content]]
+  end
+end
+
+
+
 end
